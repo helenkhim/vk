bbca530e4e9c581c990feb8ee977f64365b2f2af
Елена <elena.bogacheva97@gmail.com>
2021-03-16 21:13:34 +0300

    postmanager

--- src/main/java/manager/PostManager.java
+++ src/main/java/manager/PostManager.java
@@ -1,2 +1,18 @@
-package manager;public class PostManager {
+package manager;
+import domain.Post;
+
+public class PostManager {
+    private Post[] posts;
+
+    public Post[] getPosts() {
+        return posts;
+    }
+
+    public Post[] search(int ownerId, String domain, String query, int ownersOnly, int count, int offset) {
+        return null;
+    }
+
+    public void delete(int ownerId, int postId) {
+        return;
+    }
 }
